724184da8837fc79e49a4ca1dde9e3547bceb740
Kevin Chang <kevin@kevinwchang.com>
2019-07-24 20:29:34 -0700

    dome working, somo test

--- r2dr.ino
+++ r2dr.ino
@@ -8,18 +8,30 @@ const int16_t LineSensorTimeout = 1000;
 const int16_t LineSensorMin = 200;
 const int16_t LineSensorMax = 1000;
 
-QTRSensorsRC lineSensors((unsigned char[]) {14, 15, 16}, NumLineSensors, LineSensorTimeout);
+const int16_t DomeSensorMin = 100;
+const int16_t DomeSensorMax = 400;
+
+QTRSensorsRC lineSensors((uint8_t[]) {14, 15, 16}, NumLineSensors, LineSensorTimeout);
 uint16_t lineSensorValues[NumLineSensors];
 
+QTRSensorsRC domeSensor((uint8_t[]) {17}, 1, LineSensorTimeout);
+uint16_t domeSensorValue;
+
 DRV8835 driveMotors(7, 5, 8, 6);
+DRV8835 domeMotor(10, 9, 10, 9);
 const int16_t MaxSpeed = 255;
 
 Encoder lwEnc(18, 19);
 Encoder rwEnc(21, 20);
+Encoder domeEnc(22, 23);
+
+// ~380:1 gearbox * 12 CPR(motor) 
+// 4550 is exact; see gear ratio at https://www.pololu.com/product/4790
+const int16_t DomeCPR = 4550; 
 
 Pushbutton btn(0);
 
-enum { WaitForButton, FindLine, StartFollowLine, FollowLine, GoHome, Done } state;
+enum { WaitForButton, FindLine, FollowLine, Decel, TurnToHome, GoHome } state;
 
 int16_t ls, rs;
 
@@ -27,8 +39,8 @@ const uint16_t AngleScale = 20000;
 const uint16_t StepsPerRadian = 1025;
 const uint8_t LeapTickPerL = 0;
 const uint8_t LeapTickPerR = 200;
-const int32_t FollowMaxY = 13000000L;
-const int16_t FollowMaxS = 15000L;
+const int32_t HomeMaxY = 13000000L;
+const int16_t HomeMaxS = 15000L;
 
 int16_t c = AngleScale;
 int16_t s = 0;
@@ -38,29 +50,43 @@ int32_t x = 0, y = 0;
 
 void followLine(int16_t accelMaxSpeed = 255);
 
+const uint8_t SomoClk = 3;
+const uint8_t SomoData = 4;
+
 void setup()
 {
   Serial.begin(115200);
-  setLineSensorCalibration();
-  driveMotors.init(23437);
+  setSensorCalibration();
+  driveMotors.init(20000);
+  domeMotor.init(20000);
   
   pinMode(13, OUTPUT);
   
-  state = WaitForButton;
+  somoInit();
+  somoCmd(1);
+  
+  homeDome();
+
+  state = WaitForButton;  
 }
 
 void loop()
 {
-  static uint16_t millisInitial = 0;
-  static boolean accel = false;
+  static uint16_t accelStartMs = 0;
+  static uint16_t stateStartMs = 0;
+  static bool accel = false;
   static int16_t accelMaxSpeed = 0;
+
+  updateDome();
   
-  if (state != WaitForButton && state != Done)
+  if (state != WaitForButton)
+  {
     updateWheelEncoders();
+  }
   
   if (accel)
   {
-    accelMaxSpeed = (millis() - millisInitial) / 4; // 0 to 255 over ~1 second
+    accelMaxSpeed = (uint16_t)(millis() - accelStartMs) / 4; // 0 to 255 over ~1 second
     if (accelMaxSpeed >= MaxSpeed)
     {
       accelMaxSpeed = MaxSpeed;
@@ -68,82 +94,208 @@ void loop()
     }
   }
   
-  switch(state)
+  switch (state)
   {
     case WaitForButton:
-    {
-      btn.waitForButton();
+    
+      if (!btn.getSingleDebouncedRelease()) { return; }
+      digitalWrite(13, HIGH);
       delay(1000);
+      
       lwEnc.write(0);
       rwEnc.write(0);
+      
       state = FindLine;
       accel = true;
-      millisInitial = millis();
-    }
-    break;
+      accelStartMs = millis();
+      
+      break;
 
     case FindLine:
-    {
+    
       driveMotors.setSpeeds(accelMaxSpeed, accelMaxSpeed);
 
-        
       lineSensors.readCalibrated(lineSensorValues);
       if (onLine())
       {
-        state = StartFollowLine;
-        millisInitial = millis();
-      }
-    }
-    break;
-      
-    case StartFollowLine:
-    {
-      followLine(accelMaxSpeed);
-      if ((millis() - millisInitial) > 2000)
-      {
         state = FollowLine;
-        digitalWrite(13, HIGH);
+        stateStartMs = millis();
       }
-    }
-    break;
+    
+      break;
     
     case FollowLine:
-    {
-      followLine();
-      if (!onLine())
+
+      followLine(accelMaxSpeed);
+
+      // to avoid thinking we're done before we're stable on the line,
+      // don't look for end of line for 1 second
+      if ((uint16_t)(millis() - stateStartMs) > 1000 && !onLine())
       {
-        printDebug();
-        Serial.println("end of line");
-        
-        transform();
-        printDebug();
-        
-        state = GoHome;
-        millisInitial = millis();
+        //Serial.println("end of line");
+        state = Decel;
+        stateStartMs = millis();
         digitalWrite(13, LOW);
       }
-    }
-    break;
+
+      break;
+
+    case Decel:
+
+      accelMaxSpeed = MaxSpeed - (uint16_t)(millis() - stateStartMs) / 2; // 255 to 0 over ~0.5 second
+      if (accelMaxSpeed < 0) { accelMaxSpeed = 0; }
+      if (ls > accelMaxSpeed) { ls = accelMaxSpeed; }
+      if (rs > accelMaxSpeed) { rs = accelMaxSpeed; }
+      driveMotors.setSpeeds(ls, rs);
+
+      if (accelMaxSpeed == 0)
+      {
+        //printDebug();
+        transform();
+        //printDebug();
+        
+        state = TurnToHome;
+        digitalWrite(13, HIGH);
+      }
+
+      break;
+
+    case TurnToHome:
+
+      turnToHome();
+      
+      if (abs(s) < (AngleScale / 18))
+      {
+        // pointing within 5 degrees of home direction
+        
+        state = GoHome;
+        accel = true;
+        accelStartMs = millis();
+        digitalWrite(13, LOW);
+      }
+
+      break;
     
     case GoHome:
-    {
-      goHome();
-      if(x > -5000000)
+
+      goHome(accelMaxSpeed);
+      
+      if (x > -5000000)
       {
-        state = Done;
+        driveMotors.setSpeeds(0, 0);
+        state = WaitForButton;
       }
-    }
-    break;
-    
-    case Done:
-    {
-      driveMotors.setSpeeds(0, 0);
-    }
-    break;
+
+      break;
   }
 }
 
-void setLineSensorCalibration()
+void homeDome()
+{
+  // find + edge:
+  domeMotor.setMASpeed(100);
+  
+  // wait for black
+  while (1)
+  {
+    domeSensor.readCalibrated(&domeSensorValue);
+    if (domeSensorValue > 500) { break; }
+  }
+
+  domeMotor.setMASpeed(50);
+
+  // wait for white
+  while (1)
+  {
+    domeSensor.readCalibrated(&domeSensorValue);
+    if (domeSensorValue < 500) { break; }
+  }
+
+  domeMotor.setMASpeed(0);
+  delay(100);
+  int16_t posP = domeEnc.read();
+
+  // find - edge:
+  domeMotor.setMASpeed(-50);
+  
+  // wait for black
+  while (1)
+  {
+    domeSensor.readCalibrated(&domeSensorValue);
+    if (domeSensorValue > 500) { break; }
+  }
+
+  // wait for white
+  while (1)
+  {
+    domeSensor.readCalibrated(&domeSensorValue);
+    if (domeSensorValue < 500) { break; }
+  }
+
+  domeMotor.setMASpeed(0);
+  delay(100);
+  int16_t posM = domeEnc.read();
+
+  // black bar is (posP - posM) wide, and we're now on the - side of it,
+  // so our current position should be:
+  domeEnc.write(-(posP - posM) / 2);
+}
+
+void updateDome()
+{
+  
+  static int8_t lastCount = 0; // last enc reading
+  static int16_t wrappedCount = 0;
+  static uint16_t lastDomeMoveMs = 0;
+  
+  int8_t count = domeEnc.read();
+  
+  wrappedCount += (int8_t)(lastCount - count);
+  lastCount = count;
+
+  
+  if (wrappedCount >  (DomeCPR/2)) { wrappedCount -= DomeCPR; }
+  if (wrappedCount < -(DomeCPR/2)) { wrappedCount += DomeCPR; }
+
+  const uint8_t MovePeriod = 40; // 40 ms = 25 Hz
+  uint16_t now = millis();
+  
+  if ((uint16_t)(now - lastDomeMoveMs) >= MovePeriod)
+  {
+    lastDomeMoveMs += MovePeriod;
+    
+    float domeTarget;
+    
+    switch (state)
+    {
+      case FindLine:
+      case FollowLine:
+      case Decel:
+      case TurnToHome:
+      case GoHome:
+        domeTarget = atan2(s, -c) + atan2(y, x);
+        break;
+
+      default:
+        domeTarget = 0;
+    }
+    
+    int16_t targetCount = domeTarget * DomeCPR / 2 / PI;
+    if (targetCount >  DomeCPR/2) { targetCount -= DomeCPR; }
+    if (targetCount < -DomeCPR/2) { targetCount += DomeCPR; }
+
+    int16_t countDiff = (wrappedCount - targetCount);
+    if (countDiff >  DomeCPR/2) { countDiff -= DomeCPR; }
+    if (countDiff < -DomeCPR/2) { countDiff += DomeCPR; }
+  
+    int16_t domeSpeed = -countDiff;
+    domeSpeed = constrain(domeSpeed, -MaxSpeed, MaxSpeed);
+  
+    domeMotor.setMASpeed(domeSpeed);
+  }
+}
+
+void setSensorCalibration()
 {
   lineSensors.calibrate(); // force allocate calibrated values
   
@@ -152,15 +304,23 @@ void setLineSensorCalibration()
     lineSensors.calibratedMinimumOn[i] = LineSensorMin;
     lineSensors.calibratedMaximumOn[i] = LineSensorMax;
   }
+
+  domeSensor.calibrate();
+  domeSensor.calibratedMinimumOn[0] = DomeSensorMin;
+  domeSensor.calibratedMaximumOn[0] = DomeSensorMax;
 }
 
 void updateWheelEncoders()
 {
-  static int8_t prevLCount = 0, prevRCount = 0;
+  static int8_t lastLCount = 0, lastRCount = 0;
   static uint8_t leapLCount = 0, leapRCount = 0;
   
-  int8_t dLCount = (int8_t)(lwEnc.read() - prevLCount);
-  int8_t dRCount = (int8_t)(rwEnc.read() - prevRCount);
+  int8_t dLCount = (int8_t)(lwEnc.read() - lastLCount);
+  int8_t dRCount = (int8_t)(rwEnc.read() - lastRCount);
+
+  Serial.print(dLCount);
+  Serial.print('\t');
+  Serial.println(dRCount);
   
   if (dLCount != 0)
   {
@@ -169,17 +329,19 @@ void updateWheelEncoders()
   
     c += dc;
     s += ds;
+    c = constrain(c, -AngleScale, AngleScale);
+    s = constrain(s, -AngleScale, AngleScale);
     x += dLCount * c;
     y += dLCount * s;
     
-    prevLCount += dLCount;
+    lastLCount += dLCount;
     
     if (LeapTickPerL > 0)
     {
       leapLCount += dLCount;
       if (leapLCount >= LeapTickPerL)
       {
-        prevLCount -= 1;
+        lastLCount -= 1;
         leapLCount -= LeapTickPerL;
       }
     }
@@ -187,33 +349,33 @@ void updateWheelEncoders()
   
   if (dRCount != 0)
   {
-    int16_t dc = - divide((int32_t)dRCount*s + (int32_t)dRCount*c/2/StepsPerRadian, StepsPerRadian);
-    int16_t ds = + divide((int32_t)dRCount*c - (int32_t)dRCount*s/2/StepsPerRadian, StepsPerRadian);
+    int16_t dc = - divide(dRCount*s + dRCount*c/2/StepsPerRadian, StepsPerRadian);
+    int16_t ds = + divide(dRCount*c - dRCount*s/2/StepsPerRadian, StepsPerRadian);
     
     c += dc;
     s += ds;
     x += dRCount * c;
     y += dRCount * s;
   
-    prevRCount += dRCount;
+    lastRCount += dRCount;
     
     if (LeapTickPerR > 0)
     {
       leapRCount += dRCount;
       if (leapRCount >= LeapTickPerR)
       {
-        prevRCount -= 1;
+        lastRCount -= 1;
         leapRCount -= LeapTickPerR;
       }
     }
   }
   
-  static uint16_t lastPrint = 0;
-  if ((uint16_t)(millis() - lastPrint) > 500)
+  /*static uint16_t lastProportionalrint = 0;
+  if ((uint16_t)(millis() - lastProportionalrint) > 500)
   {
     printDebug();
-    lastPrint = millis();
-  }
+    lastProportionalrint = millis();
+  }*/
 }
 
 int32_t divide(int32_t a, int32_t b)
@@ -234,29 +396,58 @@ void transform()
   x = -r;
 }
 
-void goHome()
+void turnToHome()
 {
-  int16_t speed = MaxSpeed;
+  const int16_t TurnMaxDiff = MaxSpeed / 2;
   int32_t err;
   
-  if(x > -20000000)
-    speed = min(MaxSpeed/2, speed);
+  if (c < 0)
+  {
+    // pointed backwards
+    err = (s > 0 ? TurnMaxDiff : -TurnMaxDiff);
+  }
+  else
+  {    
+    int32_t target_s = -(y / 10000 * HomeMaxS / (HomeMaxY / 10000));
+    target_s = constrain(target_s, -HomeMaxS, HomeMaxS);
+    err = (s - target_s) / 20;
+    err = constrain(err, -TurnMaxDiff, TurnMaxDiff);
+  }
   
-  if(c < 0)
+  driveMotors.setSpeeds(err, -err);
+}
+
+void goHome(int16_t accelMaxSpeed)
+{
+  int16_t speed = accelMaxSpeed;
+  int32_t err;
+  
+  if (x > -20000000)
+  {
+    speed /= 4;
+  }
+  
+  /*if (c < 0)
   {
     // pointed backwards
     err = (s > 0 ? speed/2 : -speed/2);
   }
-  else
+  else*/
   {    
-    int32_t target_s = -max(min(y / 10000 * FollowMaxS / (FollowMaxY / 10000), FollowMaxS), -FollowMaxS);
-    err = (s - target_s)/100;
-    err = max(min(err,speed),-speed);
+    int32_t target_s = -(y / 10000 * HomeMaxS / (HomeMaxY / 10000));
+    target_s = constrain(target_s, -HomeMaxS, HomeMaxS);
+    err = (s - target_s) / 100;
+    err = constrain(err, -speed, speed);
   }
-  if(err > 0)
+  
+  if (err > 0)
+  {
     driveMotors.setSpeeds(speed, speed - err);
+  }
   else
+  {
     driveMotors.setSpeeds(speed + err, speed);
+  }
 }
 
 boolean onLine()
@@ -266,9 +457,9 @@ boolean onLine()
 
 void followLine(int16_t accelMaxSpeed)
 {
-  static uint16_t last_proportional = 0;
+  static uint16_t lastProportional = 0;
   static long integral = 0;
-  static const int16_t lfMaxSpeed = accelMaxSpeed, lfMaxDiff = accelMaxSpeed;
+  const int16_t LfMaxSpeed = accelMaxSpeed, LfMaxDiff = accelMaxSpeed;
   
   // Get the position of the line.  Note that we *must* provide
   // the "sensors" argument to read_line() here, even though we
@@ -280,11 +471,11 @@ void followLine(int16_t accelMaxSpeed)
 
   // Compute the derivative (change) and integral (sum) of the
   // position.
-  int16_t derivative = proportional - last_proportional;
+  int16_t derivative = proportional - lastProportional;
   integral += proportional;
 
   // Remember the last position.
-  last_proportional = proportional;
+  lastProportional = proportional;
 
   // Compute the difference between the two motor power settings,
   // m1 - m2.  If this is a positive number the robot will turn
@@ -295,20 +486,18 @@ void followLine(int16_t accelMaxSpeed)
 
   // Compute the actual motor settings.  We never set either motor
   // to a negative value.
-  if(power_difference > lfMaxDiff)
-    power_difference = lfMaxDiff;
-  if(power_difference < -lfMaxDiff)
-    power_difference = -lfMaxDiff;
+  if (power_difference >  LfMaxDiff) { power_difference =  LfMaxDiff; }
+  if (power_difference < -LfMaxDiff) { power_difference = -LfMaxDiff; }
 
-  if(power_difference < 0)
+  if (power_difference < 0)
   {
-    ls = lfMaxSpeed + power_difference;
-    rs = lfMaxSpeed;
+    ls = LfMaxSpeed + power_difference;
+    rs = LfMaxSpeed;
   }
   else
   {
-    ls = lfMaxSpeed;
-    rs = lfMaxSpeed - power_difference;
+    ls = LfMaxSpeed;
+    rs = LfMaxSpeed - power_difference;
   }
   driveMotors.setSpeeds(ls, rs);
 }
@@ -322,5 +511,30 @@ void printDebug()
   Serial.print(x); Serial.print("\t");
   Serial.print(y); Serial.print("\t");
   Serial.println();
-  
+}
+
+void somoInit()
+{
+    pinMode(SomoClk, OUTPUT);
+    digitalWrite(SomoClk, HIGH);
+    pinMode(SomoData, OUTPUT);
+    digitalWrite(SomoData, HIGH);
+    
+    while (millis() < 1000) {}
+}
+
+void somoCmd(uint16_t cmd)
+{   
+    digitalWrite(SomoClk, LOW);
+    delay(2);
+    
+    for (int8_t b = 15; b >= 0; b--)
+    {
+        digitalWrite(SomoClk, LOW);
+        digitalWrite(SomoData, (cmd >> b) & 1);
+        delayMicroseconds(90);
+        digitalWrite(SomoClk, HIGH);
+        delayMicroseconds(90);
+    }
+    delay(2);
 }
